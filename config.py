95ae3fea0ed80649a8532c22e54cb6d53f391d47
Dima8426 <dmitriy26-84@mail.ru>
2023-03-23 15:42:43 +0300

    Initial commit

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-user_token = 'vk1.a.cKOqIaS9WqoIy0CeTWfMq_vyDwE5HYLTFFGAqeQxM-pnWdKQTiVy6ezpZOvyByrC8CFQd0QMlLTwNbV4y68jpLjt_Q5RbbM9oRQLaBsPgv-NwUSFDIWcc602TCpd-d3mI7PPOUVySDvnOprDxz3WzCZ2gLA1zpioajbUnvupeuc5Pxxsbcl_nksYc0qyU1SyaW7k_uw9RxEdaFmQ5dM3ww' # токен пользователя
-group_token = 'vk1.a.cI9Fg0k1ag56KUuADihbyqA8jtHdSNCei4aqsbDG5NAPO9lFvLBVwZ5w09ODk0ia8B50mNcpB3tSR3rF5m5iu3vktFb2hUhJtS-W5yEFiPJAY-X21XV5TipxkQCIAWsj3xbZLycN5-WCuhumuXpRYsyDsWO2knPxzp81wPsMxmqv43YJVqTT9MBI2I1U7ufs5ztkO7wYFr0YMLWZYmBsOg'
-offset = 0
-line = range(0, 1000)
+user_token = ''
+group_token = ''
+
 
 host = '127.0.0.1'
 user = 'postgres'
-password = 'q1w2e3r4'
+password = ''
 db_title = 'bot_users'
